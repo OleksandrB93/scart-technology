05636cc0c8ba47f7deae9238158052b6261520f9
OleksandrB93 <barabashsan4ik@gmail.com>
2025-08-13 09:05:50 +0300

    Update Button component styles for disabled state and improve text formatting

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -25,18 +25,22 @@ const Button = ({
         width: width ? width : "100%",
         background: background
           ? background
+          : disabled
+          ? "none"
           : "linear-gradient(to bottom, #6DBF1D 0%, #498013 100%)",
       }}
       className={`flex items-center justify-center gap-1 max-h-[53px] sm:gap-2 rounded-lg  py-2.5 transition-all duration-200 ${
         disabled
-          ? "text-white/40 border-white/5 cursor-not-allowed pointer-events-none border border-dashed"
+          ? "text-white/40 border-white/20 cursor-not-allowed pointer-events-none border border-dashed"
           : "text-white/80  border-t border-white/40 hover:text-white/60 hover:border-white/20 cursor-pointer"
       }`}
       onClick={onClick}
       // disabled={disabled}
     >
       {iconSide === "left" && icon}
-      <p className="font-extrabold text-white/80 text-[12px] sm:text-[18px]">{text}</p>
+      <p className="font-extrabold text-white/80 text-[12px] sm:text-[18px]">
+        {text}
+      </p>
       {iconSide === "right" && icon}
       {iconSide === "right" && btnValue && (
         <span className="font-extrabold text-white/80 text-[12px] sm:text-[18px]">
